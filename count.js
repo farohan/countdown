e04faa5640f2fd3d00a02927d0a5540e81b7605c
farohan <68710283+farohan@users.noreply.github.com>
2021-08-25 21:21:38 -0700

    Create count.js

---
+++ count.js
@@ -0,0 +1,55 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
+function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it!
+    if(secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display it
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60); //finding minutes; for example 123 seconds / 60 = Math.floor(2);
+  const remainderSeconds = seconds % 60; //finding remaining seconds using modulus
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`; 
+  document.title = display; //Setting document title to time
+  timerDisplay.textContent = display; //Never used .textContent before
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour; //Military time to normal 12-hour time
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds); //initiating everything
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer)); //Putting event listeners on the buttons
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins * 60);
+  this.reset();
+}); //Setting event listener on form
